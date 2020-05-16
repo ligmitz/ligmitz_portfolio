f4c033a9f9d0ab69462b1c39209f2e59d71e7c3e
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-05-17 00:37:15 +0530

    work on tabs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,12 +3,12 @@ import { tab_handle } from './imports/tab';
 
 window.addEventListener('load',()=>{
     let tabs = document.querySelectorAll('.nav .link li .tab');
-    tabs[0].classList.add('active');
     tabs.forEach(tab => {
         tab.addEventListener('click',()=>{
            tab_handle(tab.id); 
         }   
         );
     });
+    tabs[0].click();
 
 })
--- src/scripts/imports/tab.js
+++ src/scripts/imports/tab.js
@@ -1,8 +1,11 @@
 export function tab_handle(id){
     var i;
     var tabs = document.querySelectorAll('.nav .link li .tab');
+    var tabcontent = document.querySelectorAll('#content .tabcontent');
     for(i = 0;i < tabs.length ; ++i){
         tabs[i].classList.remove('active');
+        tabcontent[i].style.display = 'none';
     }
+    tabcontent[id].style.display = 'flex';
     tabs[id].classList.add('active');
 }
