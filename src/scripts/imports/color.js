98b5d6cd5e5b37713cfd696c0360014fd994261b
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-07-25 12:03:05 +0530

    code sort

--- src/scripts/imports/color.js
+++ src/scripts/imports/color.js
@@ -12,34 +12,37 @@ export function create_cards() {
     })
     .then((data) => {
       for (var i = 0; i < data.length; i++) {
-        let card = document.createElement("div");
-        let header = document.createElement("div");
-        let title = document.createElement("div");
-        let link = document.createElement("a");
-        let author = document.createElement("div");
-        let desc = document.createElement("div");
-        let item_color = color_picker();
-
-        card.classList.add("card");
-        header.classList.add("header");
-        title.classList.add("title");
-        author.classList.add("author");
-        desc.classList.add("descriptor");
-
-        title.appendChild(link);
-        header.appendChild(title);
-        header.appendChild(author);
-        card.appendChild(header);
-        card.appendChild(desc);
-        cards.appendChild(card);
-
-        link.textContent = data[i].name;
-        author.textContent = data[i].owner.login;
-        link.setAttribute("target", "blank");
-        link.setAttribute("href", data[i].html_url);
-
-        card.style.color = item_color;
-        link.style.color = item_color;
+        if(data[i].stargazers_count !== 0){
+          let card = document.createElement("div");
+          let header = document.createElement("div");
+          let title = document.createElement("div");
+          let link = document.createElement("a");
+          let author = document.createElement("div");
+          let desc = document.createElement("div");
+          let item_color = color_picker();
+  
+          card.classList.add("card");
+          header.classList.add("header");
+          title.classList.add("title");
+          author.classList.add("author");
+          desc.classList.add("descriptor");
+  
+          title.appendChild(link);
+          header.appendChild(title);
+          header.appendChild(author);
+          card.appendChild(header);
+          card.appendChild(desc);
+          cards.appendChild(card);
+  
+          link.textContent = data[i].name;
+          author.textContent = data[i].owner.login;
+          desc.textContent = data[i].description;
+          link.setAttribute("target", "blank");
+          link.setAttribute("href", data[i].html_url);
+  
+          card.style.color = item_color;
+          link.style.color = item_color;
+        }
       }
     });
 
